d4c3bab40ba9bedd92156f1a302014138a0edc48
Jason Harris <sirmanlypowers@gmail.com>
2022-10-05 16:51:59 -0700

    Working linux script for ipip tunnels.

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-LOCAL_IPV4_ADR=192.168.1.21
-REMOTE_IPV4_ADR=192.168.1.22
+LOCAL_IPV4_ADR=192.168.1.22
+REMOTE_IPV4_ADR=192.168.1.21
+
+LOCAL_TUN_IPV4_ADR=10.0.0.1
+REMOTE_TUN_IPV4_ADR=10.0.0.2
+
 PHYS_DEV=wlp8s0
 TUN_DEV=tun0
-TUN_IPV4_ADR=10.0.0.1
-MODE=sit
+
+MODE=ipip
 
 # remove any pre-existing tunnel interface
 ip link set $TUN_DEV down
@@ -13,6 +17,5 @@ ip tunnel del $TUN_DEV
 
 # setup a new tunnel interface
 ip tunnel add $TUN_DEV mode $MODE local $LOCAL_IPV4_ADR remote $REMOTE_IPV4_ADR dev $PHYS_DEV
-ip addr add $TUN_IPV4_ADR/24 dev $TUN_DEV
+ip addr add $LOCAL_TUN_IPV4_ADR/24 dev $TUN_DEV
 ip link set $TUN_DEV mtu 1500 up
-
