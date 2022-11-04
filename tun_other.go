a099b5569c226e498ab33e17a116be207557497d
Ludi Rehak <ludi317@gmail.com>
2022-11-04 14:01:15 -0700

    Add stubs for non-linux, non-freebsd platforms to fix builds on MacOS.

--- tun_other.go
+++ tun_other.go
@@ -2,8 +2,47 @@
 
 package tuntap
 
+import (
+	"net"
+)
+
 const flagTruncated = 0
 
 func createInterface(ifPattern string, kind DevKind) (*Interface, error) {
+	panic("tuntap: Not implemented on this platform")
+}
+
+// IPv6SLAAC enables/disables stateless address auto-configuration (SLAAC) for the interface.
+func (t *Interface) IPv6SLAAC(ctrl bool) error {
+	panic("tuntap: Not implemented on this platform")
+}
+
+// IPv6Forwarding enables/disables ipv6 forwarding for the interface.
+func (t *Interface) IPv6Forwarding(ctrl bool) error {
+	panic("tuntap: Not implemented on this platform")
+}
+
+// IPv6 enables/disable ipv6 for the interface.
+func (t *Interface) IPv6(ctrl bool) error {
+	panic("tuntap: Not implemented on this platform")
+}
+
+// AddAddress adds an IP address to the tunnel interface.
+func (t *Interface) AddAddress(ip net.IP, subnet *net.IPNet) error {
+	panic("tuntap: Not implemented on this platform")
+}
+
+// SetMTU sets the tunnel interface MTU size.
+func (t *Interface) SetMTU(mtu int) error {
+	panic("tuntap: Not implemented on this platform")
+}
+
+// Up sets the tunnel interface to the UP state.
+func (t *Interface) Up() error {
+	panic("tuntap: Not implemented on this platform")
+}
+
+// GetAddrList returns the IP addresses (as bytes) associated with the interface.
+func (t *Interface) GetAddrList() ([][]byte, error) {
 	panic("tuntap: Not implemented on this platform")
 }
