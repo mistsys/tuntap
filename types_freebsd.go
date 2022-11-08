d4fec7282cab71bed43310c740cfcd0bacbc931c
Jason Harris <sirmanlypowers@gmail.com>
2022-11-08 15:17:51 -0800

    Added some more ipv6 freebsd defines.

--- types_freebsd.go
+++ types_freebsd.go
@@ -24,12 +24,19 @@ const sizeofIfreq = C.sizeof_struct_ifreq
 const sizeofIn6AliasReq = C.sizeof_struct_in6_aliasreq
 const sizeofIn6SockAddr = C.sizeof_struct_sockaddr_in6
 const sizeofIn6AddrLifetime = C.sizeof_struct_in6_addrlifetime
+const sizeofNdIfInfo = C.sizeof_struct_nd_ifinfo
 
 const (
-	IFNAMSIZ              = C.IFNAMSIZ
-	ND6_INFINITE_LIFETIME = C.ND6_INFINITE_LIFETIME
-	SIOCDIFADDR_IN6       = C.SIOCDIFADDR_IN6
-	SIOCAIFADDR_IN6       = C.SIOCAIFADDR_IN6
+	IFNAMSIZ                 = C.IFNAMSIZ
+	ND6_INFINITE_LIFETIME    = C.ND6_INFINITE_LIFETIME
+	SIOCDIFADDR_IN6          = C.SIOCDIFADDR_IN6
+	SIOCAIFADDR_IN6          = C.SIOCAIFADDR_IN6
+	ND6_IFF_DONT_SET_IFROUTE = C.ND6_IFF_DONT_SET_IFROUTE
+	ND6_IFF_AUTO_LINKLOCAL   = C.ND6_IFF_AUTO_LINKLOCAL
+	ND6_IFF_NO_RADR          = C.ND6_IFF_NO_RADR
+	ND6_IFF_NO_PREFER_IFACE  = C.ND6_IFF_NO_PREFER_IFACE
+	ND6_IFF_NO_DAD           = C.ND6_IFF_NO_DAD
+	SIOCSIFINFO_FLAGS        = C.SIOCSIFINFO_FLAGS
 
 	// tun
 	TUNSDEBUG  = C.TUNSDEBUG
